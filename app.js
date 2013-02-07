e935fec3c756ee1600e44593daaa14447c89eb56
Dan McGee <dpmcgee@gmail.com>
2013-02-06 20:00:43 -0600

    Initial app development
    * Remove example /users route
    * Add a quick middleware to store connected clients by IP in Redis

--- app.js
+++ app.js
@@ -5,11 +5,18 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , redis = require('redis');
+
+var db = redis.createClient();
+db.select(6);
 
 var app = express();
+
+function ipTracker(req, res, next){
+  db.zadd('online', Date.now(), req.ip, next);
+}
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -20,6 +27,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(ipTracker);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -29,7 +37,6 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
