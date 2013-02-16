f9c0f607412797a692ef5dd9b746fab9c697a8c6
Dan McGee <dpmcgee@gmail.com>
2013-02-16 17:11:41 -0600

    Make redis helper production-ready

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,8 +1,33 @@
 var redis = require('redis');
 
+function throwError(err) {
+  if (err) {
+    throw err;
+  }
+}
+
+function logError(err) {
+  console.log(err);
+}
+
 function getConnection() {
-  var db = redis.createClient();
-  db.select(6, function (err) { if (err) throw err; });
+  var port = process.env.REDIS_PORT || 6379,
+      host = process.env.REDIS_HOST || '127.0.0.1',
+      password = process.env.REDIS_PASS,
+      dbNumber = process.env.REDIS_DB;
+  var db = redis.createClient(port, host);
+  db.on('error', logError);
+
+  var callback = throwError;
+  if (dbNumber) {
+    callback = function() { db.select(dbNumber, throwError); };
+  }
+
+  if (password) {
+    db.auth(password, callback);
+  } else {
+    callback();
+  }
   return db;
 }
 
